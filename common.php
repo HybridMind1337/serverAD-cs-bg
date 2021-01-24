5703a2c454f10835efd62340240180943c0d9b99
Enriko Todorov <impossiblelimner@gmail.com>
2021-01-24 23:44:53 +0000

    Update common.php

--- common.php
+++ common.php
@@ -202,3 +202,114 @@ function mobio_checkcode($servID, $code, $debug = 0): int
     }
     return $ret;
 }
+
+// Странициране
+function pagination($results, $properties = [])
+{
+    $defaultProperties = [
+        'get_vars' => [],
+        'per_page' => 15,
+        'per_side' => 4,
+        'get_name' => 'page'
+    ];
+
+    foreach ($defaultProperties as $name => $default) $properties[$name] = (isset($properties[$name])) ? $properties[$name] : $default;
+
+    foreach ($properties['get_vars'] as $name => $value) {
+        if (isset($_GET[$name]) && ($name != $properties['get_name'])) $GETItems[] = $name . '=' . $value;
+    }
+    $l = (empty($GETItems)) ? '?' . $properties['get_name'] . '=' : '?' . implode('&', $GETItems) . '&' . $properties['get_name'] . '=';
+
+    $totalPages = ceil($results / $properties['per_page']);
+    $currentPage = (isset($_GET[$properties['get_name']]) && ($_GET[$properties['get_name']] > 1)) ? $_GET[$properties['get_name']] : 1;
+    $currentPage = ($currentPage > $totalPages) ? $totalPages : $currentPage;
+
+    $previousPage = $currentPage - 1;
+    $nextPage = $currentPage + 1;
+
+    // calculate which pages to show
+    if ($totalPages > ($properties['per_side'] * 2) + 1) {
+        $loopStart = $currentPage - $properties['per_side'];
+        $loopRange = $currentPage + $properties['per_side'];
+
+        $loopStart = ($loopStart < 1) ? 1 : $loopStart;
+        while ($loopRange - $loopStart < $properties['per_side'] * 2) {
+            $loopRange++;
+        }
+
+        $loopRange = ($loopRange > $totalPages) ? $totalPages : $loopRange;
+        while ($loopRange - $loopStart < $properties['per_side'] * 2) {
+            $loopStart--;
+        }
+    } else {
+        $loopStart = 1;
+        $loopRange = $totalPages;
+    }
+
+    // start placing data to output
+    $output = '';
+    $output .= '
+	<div class="text-center">
+	  <ul class="pagination">
+	 ';
+
+
+    // first and previous page
+    if ($currentPage != 1) {
+        $output .= '<li class="page-item"><a class="page-link" href=\'' . $l . '1\'>&#171;</a></li>';
+        $output .= '<li class="page-item"><a class="page-link" href=\'' . $l . $previousPage . '\'>‹</a></li>';
+    } else {
+        $output .= '<li class="page-item"><span class=\'page-link inactive\'>&#171;</span></li>';
+        $output .= '<li class="page-item"><span class=\'page-link inactive\'>‹</span></li>';
+    }
+
+
+    // add the pages
+    for ($p = $loopStart; $p <= $loopRange; $p++) if ($p != $currentPage) {
+        $output .= '<li class="page-item"><a class="page-link" href=\'' . $l . $p . '\'>' . $p . '</a></li>';
+    } else {
+        $output .= '<li class="page-item"><a class="page-link" href="#">' . $p . '</a></li>';
+    }
+    // next and last page
+    if ($currentPage != $totalPages) {
+        $output .= '<li class="page-item"><a class="page-link" href=\'' . $l . $nextPage . '\' class=\'active\'>›</a></li>';
+        $output .= '<li class="page-item"><a class="page-link" href=\'' . $l . $totalPages . '\' class=\'active\'>&#187;</a></li>';
+    } else {
+        $output .= '<li class="page-item"><span class=\'page-link inactive\'>›</span></li>';
+        $output .= '<li class="page-item"><span class=\'page-link inactive\'>&#187;</span></li>';
+    }
+
+    $output .= '</ul>
+   </div>';
+    // end of output
+
+    return [
+        'limit' => [
+            'first' => $previousPage * $properties['per_page'],
+            'second' => $properties['per_page']
+        ],
+
+        'output' => $output
+    ];
+}
+
+function mysqli_result($result, $row, $field = 0)
+{
+    if ($result === false) return false;
+    if ($row >= mysqli_num_rows($result)) return false;
+    if (is_string($field) && !(strpos($field, ".") === false)) {
+        $t_field = explode(".", $field);
+        $field = -1;
+        $t_fields = mysqli_fetch_fields($result);
+        for ($id = 0; $id < mysqli_num_fields($result); $id++) {
+            if ($t_fields[$id]->table == $t_field[0] && $t_fields[$id]->name == $t_field[1]) {
+                $field = $id;
+                break;
+            }
+        }
+        if ($field == -1) return false;
+    }
+    mysqli_data_seek($result, $row);
+    $line = mysqli_fetch_array($result);
+    return isset($line[$field]) ? $line[$field] : false;
+}
